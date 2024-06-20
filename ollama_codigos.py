8e57767d17239b9709ae91ae05e961d8c57ca6e6
Vitor Silva Ribas de Oliveira <94410917+vitorribas0@users.noreply.github.com>
2024-06-20 17:02:10 -0300

    Update ollama_codigos.py

--- ollama_codigos.py
+++ ollama_codigos.py
@@ -58,9 +58,9 @@ st.title("Chat com OpenAI")
 c.execute("SELECT * FROM conversation_history")
 for row in c.fetchall():
     if row[0] == "🙎‍♂:":
-        st.write(f'<div style="border: 1px solid #ccc; padding: 10px; border-radius: 10px; background-color: #f0f0f0; float: right">{row[1]}</div>', unsafe_allow_html=True)
+        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: right">{row[1]}</div>', unsafe_allow_html=True)
     else:
-        st.write(f'<div style="border: 1px solid #ccc; padding: 10px; border-radius: 10px; background-color: #f0f0f0; float: left">{row[1]}</div>', unsafe_allow_html=True)
+        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: left">{row[1]}</div>', unsafe_allow_html=True)
 
 # Fechar a conexão com o banco de dados
 conn.close()
