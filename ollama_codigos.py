d06d56f635dba84ddb6f670412de051959d31b4e
Vitor Silva Ribas de Oliveira <94410917+vitorribas0@users.noreply.github.com>
2024-06-20 17:08:57 -0300

    Update ollama_codigos.py

--- ollama_codigos.py
+++ ollama_codigos.py
@@ -2,23 +2,19 @@ import streamlit as st
 import sqlite3
 from openai import OpenAI
 
-st.set_page_config(layout="wide")  # Configuração para layout de página amplo
+st.set_page_config(layout="wide")
 
-# Inicialize o cliente OpenAI
 client = OpenAI(
     api_key="LL-rZdxy5UFL4evTVeC6H1Jzuph00H08neiKQUGm3HSYOm1qMD4T8YxonRYedIH6856",
     base_url="https://api.llama-api.com"
 )
 
-# Conexão com o banco de dados SQLite
 conn = sqlite3.connect('chat_history.db')
 c = conn.cursor()
 
-# Criar a tabela se não existir
 c.execute('''CREATE TABLE IF NOT EXISTS conversation_history 
              (role text, message text)''')
 
-# Função para enviar mensagem e obter resposta
 def enviar_mensagem(pergunta, contexto):
     messages = [{"role": "system", "content": "Olá! Sou um especialista em Python, Pandas, PySpark e AWS."}]
     messages.extend(contexto)
@@ -29,15 +25,12 @@ def enviar_mensagem(pergunta, contexto):
     )
     return response.choices[0].message.content, messages
 
-# Interface Streamlit para envio de pergunta
 pergunta = st.chat_input("Digite sua pergunta para a IA:")
 
-# Botão para limpar o histórico de conversas
 if st.button("Limpar Histórico de Conversas"):
     c.execute("DELETE FROM conversation_history")
     conn.commit()
 
-# Enviar a pergunta para a IA quando o usuário enviar a mensagem
 if pergunta:
     c.execute("SELECT * FROM conversation_history")
     contexto = [{"role": row[0], "content": row[1]} for row in c.fetchall()]
@@ -47,20 +40,16 @@ if pergunta:
     c.execute("INSERT INTO conversation_history VALUES (?, ?)", ("🤖:", resposta))
     conn.commit()
 
-# Barra lateral
-st.sidebar.title(" LLAMA 2")  # Título na barra lateral
-# Adicionando uma descrição na barra lateral
-st.sidebar.markdown("Este é um projeto feito utilizando a OpenAI.")
+st.sidebar.title(" LLAMA 2")
+st sidebar.markdown("Este é um projeto feito utilizando a OpenAI.")
 
 st.title("Chat com OpenAI")
 
-# Carregar e exibir o histórico de conversa do banco de dados
 c.execute("SELECT * FROM conversation_history")
 for row in c.fetchall():
     if row[0] == "🙎^{😊:":
-        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: left">{row[1]}</div>', unsafe_allow_html=True)
+        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: right">{row[1)}</div>', unsafe_allow_html=True)
     else:
-        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: left">{row[1']}</div>', unsafe_allow_html=True)
+        st.write(f'<div style="background-color: #87CEEB; padding: 10px; border-radius: 10px; color: #FFFFFF; float: left">{row[1]}</div>', unsafe_allow_html=True)
 
-# Fechar a conexão com o banco de dados
 conn.close()
